f8378bf7dc40805c94a6b58c502e1df7c4dd0cf5
parth radadiya <parth.radadiya@spectusinfotech.com>
2022-03-14 18:41:19 +0530

    --Create PRHandler

---
+++ TestLib/PRHandler.swift
@@ -0,0 +1,23 @@
+//
+//  PRHandler.swift
+//  TestLib
+//
+//  Created by Spectus Infotech on 14/03/22.
+//
+
+import Foundation
+import UIKit
+
+class PRHandler:UIViewController{
+    public func gotoPushViewController(){
+        //let nextVC = UIViewController()
+//        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+//        if let nextVC = storyBoard.instantiateViewController(withIdentifier: "") as? nextVC {
+            self.navigationController?.pushViewController(UIViewController(), animated: true)
+       // }
+        
+    }
+    public func goToPopViewController(){
+        self.navigationController!.popToViewController(UIViewController(), animated: true)
+    }
+}
